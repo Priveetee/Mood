b0cf1e8a708ae50ad520093fab5a28a9ff40aaf0
Priveetee <brahim.benyoucefpro@proton.me>
2025-11-02 08:00:18 +0100

    chore: remove obsolete debug files and logs

--- src/components/providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { NuqsAdapter } from "nuqs/adapters/next/app";
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <NuqsAdapter>{children}</NuqsAdapter>;
-}
