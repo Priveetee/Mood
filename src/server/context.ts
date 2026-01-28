0f445178843f303cc382034f27bf1f7cf637a849
Priveetee <brahim.benyoucefpro@proton.me>
2026-01-28 15:34:54 +0100

    fix: update auth and context for next 16 async context

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,28 +1,33 @@
 import { auth } from "@/auth";
 import prisma from "@/lib/prisma";
+import { headers } from "next/headers";
 
 interface CreateContextOptions {
   req: Request;
+  session?: typeof auth.$Infer.Session | null;
 }
 
-// CONTEXTE POUR LES ROUTES PROTÉGÉES (ADMIN)
-export async function createContext({ req }: CreateContextOptions) {
-  const session = await auth.api.getSession({
-    headers: req.headers,
-  });
+export async function createContext({ req, session }: CreateContextOptions) {
+  const finalSession =
+    session !== undefined
+      ? session
+      : await auth.api.getSession({
+          headers: await headers(),
+        });
 
   return {
-    session,
+    session: finalSession,
     prisma,
   };
 }
+
 export type Context = Awaited<ReturnType<typeof createContext>>;
 
-// CONTEXTE POUR LES ROUTES PUBLIQUES (VOTE)
 export async function createPublicContext() {
   return {
     session: null,
     prisma,
   };
 }
+
 export type PublicContext = Awaited<ReturnType<typeof createPublicContext>>;
